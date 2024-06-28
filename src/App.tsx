7e9a1212ab7095811112173538d5f115cf27a3e7
ChaseRensberger <chasehrensberger@gmail.com>
2024-06-28 15:41:19 -0400

    button not working but everything else is

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import { addCircle, updateCircle } from "./utils/mapLogic";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -8,87 +9,73 @@ const lat = 38.8898163351636;
 const lng = -77.010104237199;
 const zoom = 12;
 
-function metersToPixelsAtMaxZoom(meters: number, latitude: number) {
-  return meters / 0.075 / Math.cos((latitude * Math.PI) / 180);
-}
-
-// function getPointInsideCircle(center: number[], radius: number) {
-//   const randomAngle = Math.random() * Math.PI * 2;
-//   const randomRadius = Math.random() * radius;
-
-//   return [
-//     center[0] + randomRadius * Math.cos(randomAngle),
-//     center[1] + randomRadius * Math.sin(randomAngle),
-//   ];
-// }
-
 function App() {
   const mapContainer = useRef(null);
   const map = useRef<mapboxgl.Map | null>(null);
 
-  // const [circleWidth, setCircleWidth] = useState(2000);
-  // const [circleCenter, setCircleCenter] = useState([lng, lat]);
+  const [circleWidth, setCircleWidth] = useState(2000);
+  const [circleCenter, setCircleCenter] = useState<[lat: number, lng: number]>([
+    lat,
+    lng,
+  ]);
+  const listenerExists = useRef(false);
 
   useEffect(() => {
     if (map.current || !mapContainer.current) return;
 
+    console.log("Creating map...");
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/chasehudson01/clxy1c84x002z01qo8ff251xj",
-      center: [lng, lat],
+      center: [...circleCenter].reverse(),
       zoom: zoom,
     });
 
-    map.current.on("load", () => {
-      map.current.addSource("circleCenter", {
-        type: "geojson",
-        data: {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [lng, lat],
-          },
-        },
-      });
+    // const handleClick = (e: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
+    //   console.log("Latitude:", e.lngLat.lat);
+    //   console.log("Longitude:", e.lngLat.lng);
+    // };
 
-      map.current.addLayer({
-        id: "circleLayer",
-        type: "circle",
-        source: "circleCenter",
-        paint: {
-          "circle-radius": {
-            stops: [
-              [0, 0],
-              [20, metersToPixelsAtMaxZoom(500, lat)], // 500 meters radius
-            ],
-            base: 2,
-          },
-          "circle-color": "#f2cc50",
-          "circle-opacity": 0.3,
-        },
-      });
-    });
-  }, []);
+    // map.current.on("click", handleClick);
+
+    // return () => {
+    //   map.current.off("click", handleClick);
+    //   map.current?.remove();
+    // };
+  }, [circleCenter]);
 
   useEffect(() => {
+    console.log("Circle center:", circleCenter, "Circle width:", circleWidth);
     if (!map.current) return;
+    console.log("Map exists...");
 
-    const handleClick = (e: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
-      console.log("Latitude:", e.lngLat.lat);
-      console.log("Longitude:", e.lngLat.lng);
-    };
-
-    map.current.on("click", handleClick);
-
-    return () => {
-      map.current.off("click", handleClick);
-    };
-  }, []);
+    if (map.current.getSource("circleCenter")) {
+      updateCircle(map, circleCenter, circleWidth / 2);
+      return;
+    }
+    if (listenerExists.current) {
+      console.log("Load listener exists...");
+      console.log("Exiting...");
+      return;
+    }
+    console.log("Adding load listener...");
+    listenerExists.current = true;
+    map.current.on("load", () => {
+      console.log("Load listener activated...");
+      addCircle(map, circleCenter, circleWidth / 2);
+    });
+  }, [circleWidth, circleCenter, listenerExists]);
 
   return (
     <main className="w-screen h-screen">
       <div ref={mapContainer} className="w-full h-full mapboxgl-canvas" />
-      <button className="p-4 bg-black text-white fixed top-2 left-2 z-20">
+      <button
+        className="p-4 bg-black text-white fixed top-2 left-2 z-20"
+        onClick={() => {
+          setCircleWidth(circleWidth * 0.5);
+          setCircleCenter([lat, lng]);
+        }}
+      >
         UPDATE CIRCLE
       </button>
     </main>
---
+++ src/utils/mapLogic.ts
@@ -0,0 +1,65 @@
+import mapboxgl from "mapboxgl";
+
+function metersToPixels(meters: number, latitude: number) {
+  return meters / 0.075 / Math.cos((latitude * Math.PI) / 180);
+}
+
+function addCircle(
+  map: mapboxgl.Map,
+  center: [lat: number, lng: number],
+  radius: number
+) {
+  console.log("Adding source...");
+  map.current.addSource("circleCenter", {
+    type: "geojson",
+    data: {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [...center].reverse(),
+      },
+    },
+  });
+  console.log("Adding layer...");
+  map.current.addLayer({
+    id: "circleLayer",
+    type: "circle",
+    source: "circleCenter",
+    paint: {
+      "circle-radius": {
+        stops: [
+          [0, 0],
+          [20, metersToPixels(radius * 2, center[0])],
+        ],
+        base: 2,
+      },
+      "circle-color": "#f2cc50",
+      "circle-opacity": 0.3,
+    },
+  });
+}
+
+function updateCircle(
+  map: mapboxgl.Map,
+  center: [lat: number, lng: number],
+  radius: number
+) {
+  console.log("Updating source...");
+  map.current.getSource("circleCenter").setData({
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [...center].reverse(),
+    },
+  });
+  console.log("Updating layer...");
+  map.current.getLayer("circleLayer").paint["circle-radius"] = {
+    stops: [
+      [0, 0],
+      [20, metersToPixels(radius * 2, center[0])],
+    ],
+    base: 2,
+  };
+}
+
+export { metersToPixels, addCircle, updateCircle };
