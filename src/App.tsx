dbe712323814387dd139700fada7fd2ea4aca712
ChaseRensberger <chasehrensberger@gmail.com>
2024-06-28 10:00:12 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import { useState, useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
+
+const lat = 38.8898163351636;
+const lng = -77.010104237199;
+const zoom = 12;
+
+function metersToPixelsAtMaxZoom(meters: number, latitude: number) {
+  return meters / 0.075 / Math.cos((latitude * Math.PI) / 180);
+}
+
+function getPointInsideCircle(center: number[], radius: number) {
+  const randomAngle = Math.random() * Math.PI * 2;
+  const randomRadius = Math.random() * radius;
+
+  return [
+    center[0] + randomRadius * Math.cos(randomAngle),
+    center[1] + randomRadius * Math.sin(randomAngle),
+  ];
+}
+
+function App() {
+  const mapContainer = useRef(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+
+  const [circleWidth, setCircleWidth] = useState(2000);
+  const [circleCenter, setCircleCenter] = useState([lng, lat]);
+
+  useEffect(() => {
+    if (map.current || !mapContainer.current) return;
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/chasehudson01/clxy1c84x002z01qo8ff251xj",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    map.current.on("load", () => {
+      map.current.addSource("circleCenter", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: circleCenter,
+          },
+        },
+      });
+
+      map.current.addLayer({
+        id: "circleLayer",
+        type: "circle",
+        source: "circleCenter",
+        paint: {
+          "circle-radius": {
+            stops: [
+              [0, 0],
+              [20, metersToPixelsAtMaxZoom(circleWidth, lat)], // 500 meters radius
+            ],
+            base: 2,
+          },
+          "circle-color": "#f2cc50",
+          "circle-opacity": 0.3,
+        },
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!map.current) return;
+
+    const handleClick = (e: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
+      console.log("Latitude:", e.lngLat.lat);
+      console.log("Longitude:", e.lngLat.lng);
+    };
+
+    map.current.on("click", handleClick);
+
+    return () => {
+      map.current.off("click", handleClick);
+    };
+  }, []);
+
+  return (
+    <main className="w-screen h-screen">
+      <div ref={mapContainer} className="w-full h-full mapboxgl-canvas" />
+      <button
+        className="p-4 bg-black text-white fixed top-2 left-2 z-20"
+        onClick={() => {
+          const newCircleCenter = getPointInsideCircle(
+            circleCenter,
+            circleWidth
+          );
+          setCircleCenter(newCircleCenter);
+          map.current.getSource("circleCenter").setData({
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: newCircleCenter,
+            },
+          });
+          setCircleWidth(2000 * 0.5);
+        }}
+      >
+        UPDATE CIRCLE
+      </button>
+    </main>
+  );
+}
+
+export default App;
