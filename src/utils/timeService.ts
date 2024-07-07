535554e6aad9cb95d95c7ce500e7f38b7a05752b
ChaseRensberger <chasehrensberger@gmail.com>
2024-07-07 15:07:58 -0400

    remove timers

--- src/utils/timeService.ts
+++ src/utils/timeService.ts
@@ -1,9 +1,9 @@
 import { CronJob } from "cron";
 
 const job = new CronJob(
-  "*/10 * * * * *", // cronTime
+  "0,20,40 * * * *", // cronTime
   function () {
-    console.log("You will see this message every 10 seconds");
+    console.log("You will see this message every time seconds");
   }, // onTick
   null, // onComplete
   true, // start
